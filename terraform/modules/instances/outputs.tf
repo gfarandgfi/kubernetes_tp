05b553490e74b7d8d0aae4692c2806d5fbe1701c
Gérard Farand <gerard.farand@gfi.world>
2020-07-09 16:58:44 +0200

    minor terraform syntax

--- terraform/modules/instances/outputs.tf
+++ terraform/modules/instances/outputs.tf
@@ -1,6 +1,6 @@
 output "all_public_info" {
   value = {
     for student_names in aws_instance.student:
-      student_names.tags => student_names.public_ip
+      tostring(student_names.tags) => student_names.public_ip
   }
 }

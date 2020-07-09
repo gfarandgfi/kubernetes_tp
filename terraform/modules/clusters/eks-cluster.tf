57bc1a8aa5725a2c5368e2c64325bc99ba5e6ec2
Gérard Farand <gerard.farand@gfi.world>
2020-07-09 18:37:41 +0200

    minor terraform syntax

--- terraform/modules/clusters/eks-cluster.tf
+++ terraform/modules/clusters/eks-cluster.tf
@@ -12,10 +12,10 @@ resource "aws_eks_cluster" "formation_kubernetes" {
 
   # Ensure that IAM Role permissions are created before and deleted after EKS Cluster handling.
   # Otherwise, EKS will not be able to properly delete EKS managed EC2 infrastructure such as Security Groups.
-  depends_on = [
-    aws_iam_role_policy_attachment.cluster_role-AmazonEKSClusterPolicy,
-    aws_iam_role_policy_attachment.cluster_role-AmazonEKSServicePolicy,
-  ]
+  # depends_on = [
+  #   aws_iam_role_policy_attachment.cluster_role-AmazonEKSClusterPolicy,
+  #   aws_iam_role_policy_attachment.cluster_role-AmazonEKSServicePolicy,
+  # ]
 }
 
 resource "aws_eks_node_group" "formation_kubernetes" {

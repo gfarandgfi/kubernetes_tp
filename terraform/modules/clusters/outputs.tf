5184ccdf493cdde47407bcc9bf4742bf99c6c613
Gérard Farand <gerard.farand@gfi.world>
2020-07-09 16:57:18 +0200

    minor terraform syntax

--- terraform/modules/clusters/outputs.tf
+++ terraform/modules/clusters/outputs.tf
@@ -10,4 +10,13 @@ output "all_certificates" {
   value = {
     for student_names in aws_eks_cluster.formation_kubernetes:
       student_names.name => student_names.certificate_authority
+  }
 }
+
+# output "node_status" {
+#   value = {
+#     for student_names in aws_eks_node_group.formation_kubernetes:
+#       student_names.id => student_names.status
+#   }
+# }
+

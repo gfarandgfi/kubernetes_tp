12c1229df57ce2e5735ee4ea6a423c655c45379f
Gérard Farand <gerard.farand@gfi.world>
2020-07-09 21:11:42 +0200

    minor terraform syntax

--- terraform/modules/clusters/eks-cluster.tf
+++ terraform/modules/clusters/eks-cluster.tf
@@ -19,35 +19,35 @@ resource "aws_eks_cluster" "formation_kubernetes" {
   # ]
 }
 
-resource "aws_eks_node_group" "formation_kubernetes" {
-  for_each        = var.student_names
-  cluster_name    = each.value
-  node_group_name = "node_group-${each.value}"
-  # Hardcoded ARN. Should not be in a future version
-  node_role_arn   = "arn:aws:iam::212063436693:role/nodeGroupRole"
-  subnet_ids      = var.subnet_id
-  # Node configuration
-  instance_types  = [var.aws_instance_type]
-  labels          = var.tags
+# resource "aws_eks_node_group" "formation_kubernetes" {
+#   for_each        = var.student_names
+#   cluster_name    = each.value
+#   node_group_name = "node_group-${each.value}"
+#   # Hardcoded ARN. Should not be in a future version
+#   node_role_arn   = "arn:aws:iam::212063436693:role/nodeGroupRole"
+#   subnet_ids      = var.subnet_id
+#   # Node configuration
+#   instance_types  = [var.aws_instance_type]
+#   labels          = var.tags
 
-  remote_access {
-    ec2_ssh_key = "formation_docker"
-  }
+#   remote_access {
+#     ec2_ssh_key = "formation_docker"
+#   }
 
-  scaling_config {
-    desired_size = 1
-    max_size     = 1
-    min_size     = 1
-  }
+#   scaling_config {
+#     desired_size = 1
+#     max_size     = 1
+#     min_size     = 1
+#   }
 
-  # Ensure that IAM Role permissions are created before and deleted after EKS Node Group handling.
-  # Otherwise, EKS will not be able to properly delete EC2 Instances and Elastic Network Interfaces.
-  depends_on = [
-    aws_eks_cluster.formation_kubernetes,
-    # aws_iam_role_policy_attachment.cluster_role-AmazonEKSClusterPolicy,
-    # aws_iam_role_policy_attachment.cluster_role-AmazonEKSServicePolicy,
-  ]
-}
+#   # Ensure that IAM Role permissions are created before and deleted after EKS Node Group handling.
+#   # Otherwise, EKS will not be able to properly delete EC2 Instances and Elastic Network Interfaces.
+#   depends_on = [
+#     aws_eks_cluster.formation_kubernetes,
+#     # aws_iam_role_policy_attachment.cluster_role-AmazonEKSClusterPolicy,
+#     # aws_iam_role_policy_attachment.cluster_role-AmazonEKSServicePolicy,
+#   ]
+# }
 
 
 # resource "aws_iam_policy" "cluster_policy" {

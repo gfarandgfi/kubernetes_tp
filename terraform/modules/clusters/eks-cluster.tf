bd1e6be148e77a209d2b2ae611ef9e21118926d8
Gérard Farand <gerard.farand@gfi.world>
2020-07-09 18:35:04 +0200

    minor terraform syntax

--- terraform/modules/clusters/eks-cluster.tf
+++ terraform/modules/clusters/eks-cluster.tf
@@ -56,7 +56,7 @@ resource "aws_iam_policy" "cluster_policy" {
   "Version": "2012-10-17",
   "Statement": [
     {
-      "Action": [*],
+      "Action": ["*"],
       "Effect": "Allow",
       "Resource": "*"
     }

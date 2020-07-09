ea49623ca773246a525af5ad4abde68cb5ed5c1a
Gérard Farand <gerard.farand@gfi.world>
2020-07-09 14:37:28 +0200

    minor terraform syntax

--- terraform/modules/clusters/eks-cluster.tf
+++ terraform/modules/clusters/eks-cluster.tf
@@ -47,6 +47,37 @@ resource "aws_eks_node_group" "formation_kubernetes" {
   ]
 }
 
+# IAM Role to allow EKS service to manage other AWS services
+resource "aws_iam_role" "cluster-role" {
+  name = "cluster-iam-role"
+
+  assume_role_policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "eks.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+POLICY
+}
+
+resource "aws_iam_role_policy_attachment" "eks-cluster-AmazonEKSClusterPolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = aws_iam_role.cluster-role.name
+}
+
+resource "aws_iam_role_policy_attachment" "eks-cluster-AmazonEKSServicePolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSServicePolicy"
+  role       = aws_iam_role.cluster-role.name
+}
+
+
 # module "eks" {
 #   source       = "terraform-aws-modules/eks/aws"
 #   student_names = var.student_names

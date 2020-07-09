b9f07adc5f902fc9e45c8d63313f356fa8b9597d
Gérard Farand <gerard.farand@gfi.world>
2020-07-09 15:19:39 +0200

    minor terraform syntax

--- terraform/modules.tf
+++ terraform/modules.tf
@@ -1,23 +1,23 @@
 # Create an instance for each student
 module "instances" {
   source              = "./modules/instances"
-  subnet_id           = module.network.instances_subnet_id
   aws_instance_type   = var.aws_instance_type
   aws_instance_ami    = var.aws_instance_ami
   aws_default_zone    = var.aws_default_zone
+  subnet_id           = module.network.instances_subnet_id
   security_group_ids  = [module.network.additional_security_group_ids]
   student_names       = var.student_names
 }
 
 module "clusters" {
   source                        = "./modules/clusters"
-  additional_security_group_ids = module.network.additional_security_group_ids
-  student_names                 = var.student_names
-  tags                          = var.tags
-  vpc_id                        = module.network.vpc_id
-  private_subnets               = module.network.cluster_subnet_id
-  subnet_id                     = module.network.cluster_subnet_id
   aws_instance_type             = var.aws_instance_type
+  additional_security_group_ids = module.network.additional_security_group_ids
+  vpc_id                        = module.network.vpc_id
+  subnet_id                     = module.network.cluster_subnet_id
+  private_subnets               = module.network.cluster_subnet_id
+  tags                          = var.tags
+  student_names                 = var.student_names
 }
 
 module "network" {

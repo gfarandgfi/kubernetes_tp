612bea245f69948476c9c686b9680c6713001814
Gérard Farand <gerard.farand@gfi.world>
2020-07-10 11:01:13 +0200

    minor terraform syntax

--- terraform/modules/clusters/eks-cluster.tf
+++ terraform/modules/clusters/eks-cluster.tf
@@ -57,24 +57,3 @@ resource "aws_eks_node_group" "formation_kubernetes" {
     aws_iam_role_policy_attachment.main-node-AmazonEC2FullAccess,
     ]
 }
-
-
-resource "aws_iam_role_policy_attachment" "main-node-AmazonEKSWorkerNodePolicy" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
-  role       = aws_iam_role.main-node.name
-}
-
-resource "aws_iam_role_policy_attachment" "main-node-AmazonEKS_CNI_Policy" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
-  role       = aws_iam_role.main-node.name
-}
-
-resource "aws_iam_role_policy_attachment" "main-node-AmazonEC2ContainerRegistryReadOnly" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
-  role       = aws_iam_role.main-node.name
-}
-
-resource "aws_iam_role_policy_attachment" "main-node-AmazonEC2FullAccess" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2FullAccess"
-  role       = aws_iam_role.main-node.name
-}

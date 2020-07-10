a911591c4e7040f96b75c59f0abdfb8a142241d4
Gérard Farand <gerard.farand@gfi.world>
2020-07-10 11:07:50 +0200

    minor terraform syntax

--- terraform/modules/clusters/locals.tf
+++ terraform/modules/clusters/locals.tf
@@ -1,7 +1,7 @@
-locals {
-  main-node-userdata = <<USERDATA
-#!/bin/bash
-set -o xtrace
-/etc/eks/bootstrap.sh --apiserver-endpoint '${aws_eks_cluster.formation_kubernetes[each.value].endpoint}' --b64-cluster-ca '${aws_eks_cluster.formation_kubernetes[each.value].certificate_authority.0.data}' '${var.cluster-name}'
-USERDATA
-}
+# locals {
+#   main-node-userdata = <<USERDATA
+# #!/bin/bash
+# set -o xtrace
+# /etc/eks/bootstrap.sh --apiserver-endpoint '${aws_eks_cluster.formation_kubernetes[each.value].endpoint}' --b64-cluster-ca '${aws_eks_cluster.formation_kubernetes[each.value].certificate_authority.0.data}' '${var.cluster-name}'
+# USERDATA
+# }

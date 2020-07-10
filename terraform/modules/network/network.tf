038e55fef82a5588611ab98c053bf5f4d66b97c8
Gérard Farand <gerard.farand@gfi.world>
2020-07-10 14:40:57 +0200

    minor terraform syntax

--- terraform/modules/network/network.tf
+++ terraform/modules/network/network.tf
@@ -1,5 +1,7 @@
 # # The following resources will create one vpc and two subnets (one for the clusters, and another for the master machines)
 
+data "aws_availability_zones" "available" {}
+
 resource "aws_vpc" "formation_kubernetes" {
   enable_dns_hostnames = true
   cidr_block           = var.vpc_cidr_block
@@ -23,11 +25,11 @@ resource "aws_subnet" "formation_kubernetes_instances" {
 }
 
 resource "aws_subnet" "formation_kubernetes_clusters" {
+  count             = length(data.aws_availability_zones.available.names)
   vpc_id            = aws_vpc.formation_kubernetes.id
-  cidr_block        = var.subnet_clusters_cidr_block
-  availability_zone = var.aws_default_zone
+  cidr_block        = "10.20.${10+count.index}.0/24"
+  availability_zone = data.aws_availability_zones.available.names
   tags              = var.tags
 
   depends_on = [aws_vpc.formation_kubernetes]
 }
-

8000d3b60174bd3e8a55f2163448f442d13c73cf
Gérard Farand <gerard.farand@gfi.world>
2020-07-13 12:11:44 +0200

    two subnets per cluster

--- terraform/modules/instances/instance.tf
+++ terraform/modules/instances/instance.tf
@@ -1,14 +1,14 @@
 # Provision a Debian 10 Buster machine 
 resource "aws_instance" "student" {
-  for_each              = var.student_names
-  tags                  = {
+  for_each                  = var.student_names
+  tags                      = {
     Name = "${each.value}"
   }
-  ami                     = var.aws_instance_ami
-  instance_type           = var.aws_instance_type
-  availability_zone       = var.aws_default_zone
-  subnet_id               = var.subnet_id
-  key_name                = "formation_docker"
-  vpc_security_group_ids  = var.security_group_ids
-  associate_public_ip_address = true
+  ami                         = var.aws_instance_ami
+  instance_type               = var.aws_instance_type
+  availability_zone           = var.aws_default_zone
+  subnet_id                   = var.subnet_id
+  key_name                    = "formation_docker"
+  vpc_security_group_ids      = var.security_group_ids
+  # associate_public_ip_address = true
 }
